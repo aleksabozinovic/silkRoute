5fa55177815687ac155f001b8e7f1c800fe8da80
aleksabozinovic <aleksabozinovic722@gmail.com>
2023-02-26 13:33:37 +0100

    commit

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ const readMoreButtonAbout = document.querySelector(
   ".read__more--button--about"
 );
 const readMoreDivAbout = document.querySelector(".read__more--div--about");
+const burgerDiv = document.querySelector(".burger__div");
+const closeBurger = document.querySelector(".close--burger__div");
+const closeBurgerImg = document.querySelector(".close--burger__div img");
+const navigationCol2 = document.querySelector(".nav--col--2");
 
+// Open Navigation
+burgerDiv.addEventListener("click", (e) => {
+  navigationCol2.classList.add("active");
+  closeBurger.classList.add("active");
+});
+
+closeBurgerImg.addEventListener("click", (e) => {
+  navigationCol2.classList.remove("active");
+  closeBurger.classList.remove("active");
+});
+
+// Open More
 readMoreButtonAbout.addEventListener("click", (e) => {
   readMoreDivAbout.classList.toggle("active");
   if (readMoreDivAbout.classList.contains("active")) {
