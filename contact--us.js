38230f718025ccee12ac6f2aa996d45db6360ce2
aleksabozinovic <aleksabozinovic722@gmail.com>
2023-03-15 01:31:33 +0100

    commit

--- contact--us.js
+++ contact--us.js
@@ -1,5 +1,6 @@
 const arrow = document.querySelector(".arrow");
 const form = document.querySelector("form");
+const buttonWrapper = document.querySelector(".button__wrapper button");
 
 arrow.addEventListener("click", (e) => {
   form.classList.toggle("active");
@@ -8,4 +9,9 @@ arrow.addEventListener("click", (e) => {
     arrow.innerHTML = `<img src="/images/Arrow up1.png" alt="arrow down" />`;
   else
     arrow.innerHTML = `<img src="/images/Arrow down1.png" alt="arrow down" />`;
+});
+
+buttonWrapper.addEventListener("click", (e) => {
+  console.log(e);
+  window.location.href("http://silkroute.lv//thankyou.html");
 });
